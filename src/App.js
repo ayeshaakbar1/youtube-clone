8a10975ca1d3c64d460d522de850513129a851d2
ayeshaakbar1 <ayesha.akbar@ignicube.com>
2023-03-03 14:52:12 +0500

    my commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Navbar, Feed,  SearchFeed } from './components';
+
+ 
+const App = () => (
+   <BrowserRouter>
+     <Box sx={{ backgroundColor: '#000'}}>
+        <Navbar  />
+        <Routes>
+
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+     </Box>
+   </BrowserRouter>
+
+);
+export default App
